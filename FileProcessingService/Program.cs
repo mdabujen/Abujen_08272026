71ba9f1b8cca3e349e9fd15d4c79ecf4202b442f
mdabujen <mdabujen@gmail.com>
2026-08-28 06:21:39 +0800

    Wire up DI, Swagger, and API key middleware pipeline in Program.cs

--- FileProcessingService/Program.cs
+++ FileProcessingService/Program.cs
@@ -1,22 +1,56 @@
+using FileProcessingService.Middleware;
+using FileProcessingService.Services;
+using FileProcessingService.Services.Interfaces;
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+
+builder.Services.AddScoped<
+    ICsvProcessingService, CsvProcessingService>();
+builder.Services.AddSingleton<
+    IFileTrackingService, FileTrackingService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "File Processing API",
+        Version = "v1",
+        Description = "Uploads CSV files, computes a column average, and tracks processing history"
+    });
+
+    options.AddSecurityDefinition("ApiKey",
+        new OpenApiSecurityScheme
+        {
+            Name = "X-Api-Key",
+            Type = SecuritySchemeType.ApiKey,
+            In = ParameterLocation.Header,
+            Description = "API key required for all endpoints. Example: X-Api-Key: your-key-here"
+        });
+
+    options.AddSecurityRequirement(document =>
+        new OpenApiSecurityRequirement
+    {
+            [new OpenApiSecuritySchemeReference("ApiKey", document)] =
+                new List<string>()
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseMiddleware<ApiKeyMiddleware>();
 
 app.MapControllers();
 
